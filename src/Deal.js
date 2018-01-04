6788664de8dd8356bb0b258e19b1f28d219e2887
Jason McCutchan <jasonmccutchan@Jasons-MacBook-Pro.local>
2018-01-04 18:41:47 -0500

    savedCoupons

--- src/Deal.js
+++ src/Deal.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Deal = (props) => (
 	<div>
-		<img src={props.deal.image_url}/>
+	{props.deal.title} | {props.deal.price} | {props.deal.url}
 	</div>
 )
+
+//Need to change the image so that it is just a thumbnail and not such a large image
 
 
 export default Deal;
