ff90a2f7e87a16be5fe72045a5fee80c91836ac0
Alex Friedman <afriedman1991@gmail.com>
2018-01-04 20:09:18 -0500

    Changes to savedDealsComp jsx DealListComp jsx and Deal jsx

--- src/Deal.js
+++ src/Deal.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 const Deal = (props) => (
 	<div>
-	{props.deal.title} | {props.deal.price} | {props.deal.url}
+		<div className="card" style={{"float": "left", "width": "25rem", "height": "30rem", "margin": "10px"}}>
+			<img className="card-img-top" src={props.deal.imgUrl} alt="Card image cap"></img>
+			<div className="card-block">
+				<h4 className="card-title">{props.deal.merchant}</h4>
+				<p className="card-text">{props.deal.title}</p>
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item">Original Price: ${props.deal.price}</li>
+					<li className="list-group-item">Discount Percent: {props.deal.discount}</li>
+				</ul>
+				<a href={props.deal.url} className="btn btn-primary" style={{"position": "absolute", "bottom": "0px"}}>Deal Link</a>
+			</div>
+		</div>
 	</div>
 )
-
-//Need to change the image so that it is just a thumbnail and not such a large image
-
 
 export default Deal;
